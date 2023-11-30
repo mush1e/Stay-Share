05ff013aabab6633979768520485b3b6599e0e16
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2023-11-30 10:57:39 -0500

    enforced auth

--- controllers/rentalsController.js
+++ controllers/rentalsController.js
@@ -6,20 +6,26 @@ const Rental = require("../models/rentalModel.js");
 router.get('/',  (req, res) => res.render("rentals", { rentals: models.getRentalsByCityAndProvince() }));
 
 router.get('/list', (req, res) => {
-    Rental.find()
-      .sort({ headline: 'asc' })
-      .then(rentals => {
-        if (!rentals) {
-          return res.status(404).send('No rentals found.');
-        }
-        console.log(rentals);
-        res.render('rentals/list', { rentals });
-      })
-      .catch(error => {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
+    if (req.session.user && req.session.user.role !== 'customer') {
+        Rental.find()
+        .sort({ headline: 'asc' })
+        .then(rentals => {
+            if (!rentals) {
+            return res.status(404).send('No rentals found.');
+            }
+            console.log(rentals);
+            res.render('rentals/list', { rentals });
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
       });
-  });
+        
+    } else {
+        res.status(401).send("You are not authorized to view this page.");
+    }
+});
+    
   
 
 
